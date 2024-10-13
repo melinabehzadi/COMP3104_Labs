d08fb7c3627e8226ab4a5ba71e523116a069b9a4
melinabehzadi <melina.behzadinejad@georgebrown.ca>
2024-10-12 21:06:13 -0400

    Add build folder and update project structure

--- node_app/__tests__/app.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../../index');
-
-describe('GET /', () => {
-  it('should return Hello World', async () => {
-    const res = await request(app).get('/');
-    expect(res.text).toBe('Hello World!');
-  });
-});
